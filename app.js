3b3795e22d74c1c27b4846b32e4afbc8c9f5efe2
nash <24046195@myrp.edu.sg>
2025-07-30 11:03:36 +0800

    nash - app get for search has some issue, tba

--- app.js
+++ app.js
@@ -230,35 +230,18 @@ app.get('/admin', checkAuthenticated, checkAdmin, (req, res) => {
 //nash
 //search hotels for admin
 app.get('/AdminSearch', (req, res) => {
-    const name = req.query.name;
-    const roomType = req.query.roomType;
+    const bookings = req.query.q;
 
-    let sql = 'SELECT * FROM bookings WHERE 1';
-    const values = [];
+    const sql = "SELECT * FROM bookings WHERE name LIKE ? LIMIT 1";
+    connection.query(sql, [%${bookings}%], (err, results) => {
+        if (err) throw err;
 
-    if (name) {
-        sql += ' AND name LIKE ?';
-        values.push('%' + name + '%');
-    }
-
-    if (roomType) {
-        sql += ' AND roomType = ?';
-        values.push(roomType);
-    }
-
-    connection.query(sql, values, (err, results) => {
-        if (err) {
-            console.error('Database query error:', err.message);
-            return res.status(500).send('Error searching for hotels');
-        }
-
-        // Send results to your search results page
-        res.render('AdminSearch', { 
-            bookings: results, 
-            search: name, 
-            roomType: roomType
-        });
-    });
+        if (results.length > 0) {
+            res.redirect(/hotels/${results[0].id});
+        } else {
+            res.send("Hotel not found");
+        }
+    });
 });
 
 //******** TODO: Insert code for logout route ********//
